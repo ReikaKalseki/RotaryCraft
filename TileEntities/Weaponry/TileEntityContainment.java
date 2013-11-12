19936d71da1efdee79318d5a139868dbb9a29263
Reika <reikasminecraft@gmail.com>
2013-11-12 18:31:54 -0500

    Hopefully fixed

--- TileEntities/Weaponry/TileEntityContainment.java
+++ TileEntities/Weaponry/TileEntityContainment.java
@@ -14,6 +14,7 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.nbt.NBTTagCompound;
