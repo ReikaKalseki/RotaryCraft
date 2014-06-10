86f09e57a21c5936a12fc2f68aeda8d84b81aa2c
Reika <reikasminecraft@gmail.com>
2014-06-10 17:34:23 -0400

    new file:   Auxiliary/Interfaces/PipeRenderConnector.java 	new file:   Models/ModelPipePump.java 	new file:   Renders/DM/RenderPipePump.java 	new file:   Textures/TileEntityTex/pipepumptex.png 	new file:   TileEntities/Auxiliary/TileEntityPipePump.java

---
+++ TileEntities/Auxiliary/TileEntityPipePump.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2014
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
+package Reika.RotaryCraft.TileEntities.Auxiliary;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+import Reika.DragonAPI.Libraries.World.ReikaWorldHelper;
+import Reika.RotaryCraft.Auxiliary.Interfaces.PipeRenderConnector;
+import Reika.RotaryCraft.Auxiliary.Interfaces.PumpablePipe;
+import Reika.RotaryCraft.Base.TileEntity.TileEntityPowerReceiver;
+import Reika.RotaryCraft.Registry.MachineRegistry;
+
+public class TileEntityPipePump extends TileEntityPowerReceiver implements PipeRenderConnector {
+
+	@Override
+	protected void animateWithTick(World world, int x, int y, int z) {
+
+	}
+
+	@Override
+	public MachineRegistry getMachine() {
+		return MachineRegistry.PIPEPUMP;
+	}
+
+	@Override
+	public boolean hasModelTransparency() {
+		return false;
+	}
+
+	@Override
+	public void updateEntity(World world, int x, int y, int z, int meta) {
+		super.updateTileEntity();
+		this.getSummativeSidedPower();
+		this.getIOSidesDefault(world, x, y, z, meta);
+
+		if (this.getTicksExisted() < 2)
+			ReikaWorldHelper.causeAdjacentUpdates(world, x, y, z);
+
+		if (power >= MINPOWER && omega >= MINSPEED) {
+			ForgeDirection dir = read.getOpposite();
+			ForgeDirection dir2 = dir.getOpposite();
+			TileEntity te = this.getAdjacentTileEntity(dir);
+			TileEntity te2 = this.getAdjacentTileEntity(dir2);
+			if (te instanceof PumpablePipe && te2 instanceof PumpablePipe) {
+				PumpablePipe p1 = (PumpablePipe)te;
+				PumpablePipe p2 = (PumpablePipe)te2;
+				int lvl1 = p1.getFluidLevel();
+				int lvl2 = p2.getFluidLevel();
+				if (p2.canTransferTo(p1, dir)) {
+					p1.transferFrom(p2, this.getTransferrableAmount(lvl2));
+				}
+			}
+		}
+	}
+
+	private int getTransferrableAmount(int amt) {
+		return Math.min(amt, omega/4);
+	}
+
+	@Override
+	public int getRedstoneOverride() {
+		return 0;
+	}
+
+	@Override
+	public boolean canConnectToPipeOnSide(ForgeDirection dir) {
+		return dir == read || dir.getOpposite() == read;
+	}
+
+}
