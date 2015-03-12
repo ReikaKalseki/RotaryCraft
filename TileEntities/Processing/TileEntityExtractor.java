2b25eb3d7a6b97afb74eb1f7be79a5a05018ccf7
Reika <reikasminecraft@gmail.com>
2015-03-12 06:45:25 -0400

    Update

--- TileEntities/Processing/TileEntityExtractor.java
+++ TileEntities/Processing/TileEntityExtractor.java
@@ -458,7 +458,7 @@ public class TileEntityExtractor extends InventoriedPowerLiquidReceiver implemen
 
 	@Override
 	public boolean isItemValidForSlot(int slot, ItemStack is) {
-		if (slot == 1 || slot == 3 || slot == 5 || slot == 7)
+		if (slot%2 == 1 && slot <= 8)
 			return false;
 		if (slot == 0)
 			return ReikaBlockHelper.isOre(is);
