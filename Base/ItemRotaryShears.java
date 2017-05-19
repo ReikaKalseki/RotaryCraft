2c825d788169992a7d410086d0074f5f36738eb2
Reika <reikasminecraft@gmail.com>
2017-05-19 18:28:49 -0400

    Update 864

--- Base/ItemRotaryShears.java
+++ Base/ItemRotaryShears.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2017
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
 package Reika.RotaryCraft.Base;
 
 import net.minecraft.block.Block;
